0c9c637af580411efc42f0b4241d3f1c633f043d
rsr986 <rsr986@gmail.com>
2024-01-01 08:04:03 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1>I am h1 tag<h1>
+ *  <div>
+ * <div>
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
